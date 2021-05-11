711badc9e85058c2587f24a5bb3dd1fcd2e23fc8
mgreen821 <55030708+mgreen821@users.noreply.github.com>
2021-05-11 10:21:35 -0600

    Add project files.

---
+++ Simon_Game_Matthew_Green/MainFrom.Designer.cs
@@ -0,0 +1,188 @@
+﻿namespace Simon_Game_Matthew_Green
+{
+    partial class MainFrom
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.btnBlue = new System.Windows.Forms.Button();
+            this.btnYellow = new System.Windows.Forms.Button();
+            this.btnRed = new System.Windows.Forms.Button();
+            this.btnGreen = new System.Windows.Forms.Button();
+            this.btnStart = new System.Windows.Forms.Button();
+            this.lblHighScore = new System.Windows.Forms.Label();
+            this.lblCurrentHighScore = new System.Windows.Forms.Label();
+            this.label1 = new System.Windows.Forms.Label();
+            this.label2 = new System.Windows.Forms.Label();
+            this.SuspendLayout();
+            // 
+            // btnBlue
+            // 
+            this.btnBlue.BackColor = System.Drawing.Color.RoyalBlue;
+            this.btnBlue.Cursor = System.Windows.Forms.Cursors.Hand;
+            this.btnBlue.FlatStyle = System.Windows.Forms.FlatStyle.Popup;
+            this.btnBlue.Location = new System.Drawing.Point(56, 251);
+            this.btnBlue.Name = "btnBlue";
+            this.btnBlue.Size = new System.Drawing.Size(100, 100);
+            this.btnBlue.TabIndex = 0;
+            this.btnBlue.Tag = "1";
+            this.btnBlue.UseVisualStyleBackColor = false;
+            this.btnBlue.Click += new System.EventHandler(this.Button_Click);
+            // 
+            // btnYellow
+            // 
+            this.btnYellow.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(255)))), ((int)(((byte)(255)))), ((int)(((byte)(192)))));
+            this.btnYellow.Cursor = System.Windows.Forms.Cursors.Hand;
+            this.btnYellow.FlatStyle = System.Windows.Forms.FlatStyle.Popup;
+            this.btnYellow.Location = new System.Drawing.Point(56, 401);
+            this.btnYellow.Name = "btnYellow";
+            this.btnYellow.Size = new System.Drawing.Size(100, 100);
+            this.btnYellow.TabIndex = 1;
+            this.btnYellow.Tag = "3";
+            this.btnYellow.UseVisualStyleBackColor = false;
+            this.btnYellow.Click += new System.EventHandler(this.Button_Click);
+            // 
+            // btnRed
+            // 
+            this.btnRed.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(255)))), ((int)(((byte)(128)))), ((int)(((byte)(128)))));
+            this.btnRed.Cursor = System.Windows.Forms.Cursors.Hand;
+            this.btnRed.FlatStyle = System.Windows.Forms.FlatStyle.Popup;
+            this.btnRed.Location = new System.Drawing.Point(208, 251);
+            this.btnRed.Name = "btnRed";
+            this.btnRed.Size = new System.Drawing.Size(100, 100);
+            this.btnRed.TabIndex = 2;
+            this.btnRed.Tag = "2";
+            this.btnRed.UseVisualStyleBackColor = false;
+            this.btnRed.Click += new System.EventHandler(this.Button_Click);
+            // 
+            // btnGreen
+            // 
+            this.btnGreen.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(192)))), ((int)(((byte)(255)))), ((int)(((byte)(192)))));
+            this.btnGreen.Cursor = System.Windows.Forms.Cursors.Hand;
+            this.btnGreen.FlatStyle = System.Windows.Forms.FlatStyle.Popup;
+            this.btnGreen.Location = new System.Drawing.Point(208, 401);
+            this.btnGreen.Name = "btnGreen";
+            this.btnGreen.Size = new System.Drawing.Size(100, 100);
+            this.btnGreen.TabIndex = 3;
+            this.btnGreen.Tag = "4";
+            this.btnGreen.UseVisualStyleBackColor = false;
+            this.btnGreen.Click += new System.EventHandler(this.Button_Click);
+            // 
+            // btnStart
+            // 
+            this.btnStart.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(255)))), ((int)(((byte)(192)))), ((int)(((byte)(192)))));
+            this.btnStart.Cursor = System.Windows.Forms.Cursors.Hand;
+            this.btnStart.FlatStyle = System.Windows.Forms.FlatStyle.Popup;
+            this.btnStart.Font = new System.Drawing.Font("Microsoft Sans Serif", 26.25F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.btnStart.Location = new System.Drawing.Point(120, 69);
+            this.btnStart.Name = "btnStart";
+            this.btnStart.Size = new System.Drawing.Size(112, 46);
+            this.btnStart.TabIndex = 4;
+            this.btnStart.Text = "Start";
+            this.btnStart.UseVisualStyleBackColor = false;
+            this.btnStart.Click += new System.EventHandler(this.Button_Click);
+            // 
+            // lblHighScore
+            // 
+            this.lblHighScore.AutoSize = true;
+            this.lblHighScore.Font = new System.Drawing.Font("Microsoft Sans Serif", 20.25F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.lblHighScore.ForeColor = System.Drawing.SystemColors.ButtonHighlight;
+            this.lblHighScore.Location = new System.Drawing.Point(77, 130);
+            this.lblHighScore.Name = "lblHighScore";
+            this.lblHighScore.Size = new System.Drawing.Size(167, 31);
+            this.lblHighScore.TabIndex = 5;
+            this.lblHighScore.Text = "High Score:";
+            // 
+            // lblCurrentHighScore
+            // 
+            this.lblCurrentHighScore.AutoSize = true;
+            this.lblCurrentHighScore.Font = new System.Drawing.Font("Microsoft Sans Serif", 20.25F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.lblCurrentHighScore.ForeColor = System.Drawing.SystemColors.ButtonHighlight;
+            this.lblCurrentHighScore.Location = new System.Drawing.Point(242, 130);
+            this.lblCurrentHighScore.Name = "lblCurrentHighScore";
+            this.lblCurrentHighScore.Size = new System.Drawing.Size(30, 31);
+            this.lblCurrentHighScore.TabIndex = 6;
+            this.lblCurrentHighScore.Text = "0";
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Font = new System.Drawing.Font("Microsoft Sans Serif", 20.25F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label1.ForeColor = System.Drawing.SystemColors.ButtonHighlight;
+            this.label1.Location = new System.Drawing.Point(77, 180);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(168, 31);
+            this.label1.TabIndex = 7;
+            this.label1.Text = "Your Score:";
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Font = new System.Drawing.Font("Microsoft Sans Serif", 20.25F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label2.ForeColor = System.Drawing.SystemColors.ButtonHighlight;
+            this.label2.Location = new System.Drawing.Point(242, 180);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(30, 31);
+            this.label2.TabIndex = 8;
+            this.label2.Text = "0";
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.BackColor = System.Drawing.Color.Black;
+            this.BackgroundImageLayout = System.Windows.Forms.ImageLayout.None;
+            this.ClientSize = new System.Drawing.Size(358, 547);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.lblCurrentHighScore);
+            this.Controls.Add(this.lblHighScore);
+            this.Controls.Add(this.btnStart);
+            this.Controls.Add(this.btnGreen);
+            this.Controls.Add(this.btnRed);
+            this.Controls.Add(this.btnYellow);
+            this.Controls.Add(this.btnBlue);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Button btnBlue;
+        private System.Windows.Forms.Button btnYellow;
+        private System.Windows.Forms.Button btnRed;
+        private System.Windows.Forms.Button btnGreen;
+        private System.Windows.Forms.Button btnStart;
+        private System.Windows.Forms.Label lblHighScore;
+        private System.Windows.Forms.Label lblCurrentHighScore;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Label label2;
+    }
+}
+
---
+++ Simon_Game_Matthew_Green/MainFrom.cs
@@ -0,0 +1,128 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace Simon_Game_Matthew_Green
+{
+    public partial class MainFrom : Form
+    {
+        public MainFrom()
+        {
+            InitializeComponent();
+        }
+      
+        private void SequenceCreator()
+        {
+            List<int> sequence = new List<int>();
+            Random randomNum = new Random();
+
+            int randomSequence = randomNum.Next(1, 5);
+            sequence.Add(randomSequence);
+
+
+            if (sequence.Count() > 0)
+            {
+                foreach (int number in sequence)
+                {
+                    Task.Delay(1000).Wait();
+                    switch (number)
+                    {
+                        case 1:
+                            btnBlue.BackColor = System.Drawing.Color.Blue;
+                            Task.Delay(1000).Wait();
+                            btnBlue.BackColor = System.Drawing.Color.RoyalBlue;
+
+                            break;
+
+                        case 2:
+                            btnRed.BackColor = System.Drawing.Color.Red;
+                            Task.Delay(1000).Wait();
+                            btnRed.BackColor = System.Drawing.Color.FromArgb(255, 128, 128);
+                            break;
+
+                        case 3:
+                            btnYellow.BackColor = System.Drawing.Color.Yellow;
+                            Task.Delay(1000).Wait();
+                            btnYellow.BackColor = System.Drawing.Color.FromArgb(255, 255, 192);
+                            break;
+
+                        case 4:
+                            btnGreen.BackColor = System.Drawing.Color.Lime;
+                            Task.Delay(1000).Wait();
+                            btnGreen.BackColor = System.Drawing.Color.FromArgb(192, 255, 192);
+                            break;
+                    }
+                }
+            }
+
+        }
+
+       
+
+        private void Button_Click(object sender, EventArgs e)
+        {
+            List<int> userSequence = new List<int>();
+            Button UserInput = (Button)sender;
+            bool gameStarted = false;
+
+            
+            switch (UserInput.Name)
+            {
+                case "btnStart":
+                        gameStarted = true;
+                        break;
+
+                    case "btnBlue":
+                    if(gameStarted == true)
+                        {
+                            btnBlue.BackColor = System.Drawing.Color.Blue;
+                            Task.Delay(500).Wait();
+                            btnBlue.BackColor = System.Drawing.Color.RoyalBlue;
+                            
+                        }
+                    
+
+                    break;
+
+                case "btnRed":
+
+                        if (gameStarted == true)
+                        {
+                            btnRed.BackColor = System.Drawing.Color.Red;
+                            Task.Delay(500).Wait();
+                            btnRed.BackColor = System.Drawing.Color.FromArgb(255, 128, 128);
+                        }
+              
+                    break;
+
+                case "btnYellow":
+                        if (gameStarted == true)
+                        {
+                            btnYellow.BackColor = System.Drawing.Color.Yellow;
+                            Task.Delay(500).Wait();
+                            btnYellow.BackColor = System.Drawing.Color.FromArgb(255, 255, 192);
+                        }
+                 
+                    break;
+
+                case "btnGreen":
+                        if (gameStarted == true)
+                        {
+                            btnGreen.BackColor = System.Drawing.Color.Lime;
+                            Task.Delay(500).Wait();
+                            btnGreen.BackColor = System.Drawing.Color.FromArgb(192, 255, 192);
+                        }
+
+                  
+                    break;
+            }
+        }
+    }
+}
